35fa7e92086be091f321813d9ed2b7b8b0680c99
Dylan Ferris <dylan@psilly.com>
2022-03-30 23:24:58 -0400

    debeep, etc

---
+++ prerender-urls.js
@@ -0,0 +1,93 @@
+module.exports = async function () {
+
+  const pages = [
+    {
+        "url": "/",
+        "title": "Polynumber",
+        "photo": "/assets/polynumber.webp"
+    },
+    {
+        "url": "/about/",
+        "title": "About Polynumber.com"
+    },
+    {
+        "url": "/edit/",
+        "title": "Polynumber Editor"
+    },
+    {
+        "url": "/polynumbers/",
+        "title": "Polynumbers",
+        "photo": "/assets/polynumber.webp"
+    },
+  ]
+
+  const polynumbers = {
+    constant: {
+      path: 'constant',
+      title: 'Constant Value',
+    },
+    line: {
+      path: 'line',
+      title: 'Line',
+    },
+    parabola: {
+      path: 'parabola',
+      title: 'Parabola',
+    },
+    'inverse-parabola': {
+      path: 'inverse-parabola',
+      title: 'Inverse Parabola',
+    },
+    hyperbola: {
+      path: 'hyperbola',
+      title: 'Hyperbola',
+    },
+    circle: {
+      path: 'circle',
+      title: 'Circle',
+    },
+    'fermat-curve': {
+      path: 'fermat-curve',
+      title: 'Fermat Curve',
+    },
+    'folium-of-descartes': {
+      path: 'folium-of-descartes',
+      title: 'Folium of Descartes',
+    },
+    'lemniscate-of-bernoulli': {
+      path: 'lemniscate-of-bernoulli',
+      title: 'Lemniscate of Bernoulli',
+    },
+    'lemniscate-of-gerono': {
+      path: 'lemniscate-of-gerono',
+      title: 'Lemniscate of Gerono',
+    },
+    quartic: {
+      path: 'quartic',
+      title: 'quartic⁴',
+      description: 'A quartic polynomial.',
+    },
+    'jumping-jack': {
+      path: 'jumping-jack',
+      title: 'Jumping Jack',
+    },
+    φ: {
+      path: 'φ',
+      title: 'Phi (φ)',
+    },
+    hippopede: {
+      path: 'hippopede',
+      title: 'Hippopede',
+    },
+  }
+
+  for (const polynumber of Object.values(polynumbers)) {
+    pages.push({
+      url: `/${polynumber.path}/`,
+      title: polynumber.title,
+      photo: `/assets/thumbnails/${polynumber.path}.webp`
+    })
+  }
+
+  return pages
+}
--- src/generate-sitemap.mjs
+++ src/generate-sitemap.mjs
@@ -3,63 +3,23 @@ import prettier from 'prettier'
 
 async function generate() {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
+
+  let urlset = ''
+  const { default: getPages } = await import('../prerender-urls.js')
+  for (const page of await getPages()) {
+    urlset += `<url>
+    <loc>https://polynumber.com${page.url}</loc>
+    <lastmod>2022-03-20</lastmod>
+  </url>`
+  }
+
   const sitemap = `
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url>
-<loc>https://polynumber.com/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/about/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/polynumbers/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/constant/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/line/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/parabola/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/hyperbola/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/fermat-curve/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/folium-of-descartes/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/lemniscate-of-bernoulli/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/lemniscate-of-gerono/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/φ/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
-<url>
-<loc>https://polynumber.com/hippopede/</loc>
-<lastmod>2021-10-01</lastmod>
-</url>
+  ${urlset}
 </urlset>
-`
+  `
+
   const formatted = prettier.format(sitemap, {
     ...prettierConfig,
     parser: 'html'
